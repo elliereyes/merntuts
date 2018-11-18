e43518373278b69f2a031bfd23aa788be58627df
Ellie <asunyan12344@gmail.com>
2018-11-18 15:18:20 +0800

    Initial

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+//route to post
 router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
 
 module.exports = router;
